be4e1884e9ac0c389b50ab76a514b54e41fda052
Nuno Grilo <nfgrilo@gmail.com>
2020-10-22 15:41:59 +0100

    Updated for iOS 14.1

--- reset_simulators.sh
+++ reset_simulators.sh
@@ -20,11 +20,11 @@ iOS=com.apple.CoreSimulator.SimRuntime.iOS
 xcrun simctl create "iPhone 8 (11.4)"              "$iPhone-8"                               "$iOS-11-4"
 xcrun simctl create "iPhone X (12.4)"              "$iPhone-X"                               "$iOS-12-4"
 xcrun simctl create "iPhone 11 (13.4)"             "$iPhone-11"                              "$iOS-13-4"
-xcrun simctl create "iPhone 11 Pro (14.0)"         "$iPhone-11-Pro"                          "$iOS-14-0"
-xcrun simctl create "iPhone SE 2 (14.0)"           "$iPhone-SE--2nd-generation-"             "$iOS-14-0"
+xcrun simctl create "iPhone 11 Pro (14.1)"         "$iPhone-11-Pro"                          "$iOS-14-1"
+xcrun simctl create "iPhone SE 2 (14.1)"           "$iPhone-SE--2nd-generation-"             "$iOS-14-1"
 
 # iPads
 xcrun simctl create "iPad Pro (10.5-inch) (11.4)"  "$iPad-Pro--10-5-inch-"                   "$iOS-11-4"
 xcrun simctl create "iPad Pro (11-inch) (12.4)"    "$iPad-Pro--11-inch-"                     "$iOS-12-4"
 xcrun simctl create "iPad Air (13.4)"              "$iPad-Air--4th-generation-"              "$iOS-13-4"
-xcrun simctl create "iPad Pro (12.9-inch) (14.0)"  "$iPad-Pro--12-9-inch---4th-generation-"  "$iOS-14-0"
+xcrun simctl create "iPad Pro (12.9-inch) (14.1)"  "$iPad-Pro--12-9-inch---4th-generation-"  "$iOS-14-1"
